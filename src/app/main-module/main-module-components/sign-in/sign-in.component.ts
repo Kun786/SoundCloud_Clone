d5a95b062d0eaed109f9dc4deb400425589759ff
fazi1live <faziislive@gmail.com>
2023-06-24 21:12:55 +0500

    add

--- src/app/main-module/main-module-components/sign-in/sign-in.component.ts
+++ src/app/main-module/main-module-components/sign-in/sign-in.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-in',
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./sign-in.component.css']
 })
 export class SignInComponent {
+
+  public signInForm : FormGroup | any;
+  constructor(
+    private readonly FormBuilder:FormBuilder
+  ) {
+    this.signInFormModel()
+  }
+
+  public signInFormModel(){
+    this.signInForm = this.FormBuilder.group({
+      email: new FormControl('',[Validators.required, Validators.email]),
+      password: new FormControl('',[Validators.required, Validators.minLength(6), Validators.maxLength(15)])
+    })
+  }
+
+  signInUser(){
+    let formValues = this.signInForm.value;
+    console.log(formValues)
+  }
 
 }
